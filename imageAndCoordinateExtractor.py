cfe598ade50cb5d55715568978786cb186bd9450
Tanimoto Ryo <rryyyoooo@i.softbank.jp>
2021-03-02 20:23:04 +0900

    R

--- imageAndCoordinateExtractor.py
+++ imageAndCoordinateExtractor.py
@@ -57,8 +57,8 @@ class ImageAndCoordinateExtractor():
         """ After implemeting makeGenerator(), self.label is padded to clip correctly. """
         self.num_class = num_class
         self.class_axis = class_axis
-        self.predicted_array = np.zeros([num_class] + list(self.label_array.shape), dtype=np.float)
-        self.counter_array = np.zeros_like(self.label_array, dtype=np.float)
+        self.predicted_array = np.ones([num_class] + list(self.label_array.shape), dtype=np.float)
+        self.counter_array = np.ones_like(self.label_array, dtype=np.float)
 
     def makeGenerator(self):
         """ Caluculate paddingForNumpy size for label and image to clip correctly. """
@@ -205,7 +205,7 @@ class ImageAndCoordinateExtractor():
         """ Usually, this method is used after all of predicted patch array is insert to self.predicted_array with insertToPredictedArray. """
 
         """ Address division by zero. """
-        self.counter_array = np.where(self.counter_array == 0, 1, self.counter_array)
+        #self.counter_array = np.where(self.counter_array == 0, 1, self.counter_array)
 
         self.predicted_array /= self.counter_array
         self.predicted_array = np.argmax(self.predicted_array, axis=self.class_axis)
